c1b44cfee7e945a57765759a88baa57e8bb74666
Jeffchua96 <jeffersonchua@pursuit.org>
2023-07-30 15:56:33 -0400

    1st commit

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,31 @@
-export default function Progress() {
-  return null;
+import React from 'react';
+
+function Progress({donations}) {
+  const getTotal = (arr) => {
+    let total = 0;
+    for (let donation of arr) {
+
+      total += donation.amount;
+    }
+    
+    return total;
+  }
+
+
+const totalAmount = getTotal(donations);
+
+  return (
+    <div className="progress">
+      <h2>`Raised ${getTotal(donations)} of <span className="total"> ${1000} `</span>
+
+    <span className="secondary">$1000</span>
+  </h2>
+      <div className="progress-bar">
+        <div className="progress-bar-amount" style={{width: `${getTotal(donations)/1000 * 100}%`}}></div>
+    </div>
+    </div>
+  );
 }
+
+
+export default Progress;
