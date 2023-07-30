9d5d658ba3badba406e5c76da08984e314a2e9ae
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-30 12:12:27 -0400

    “submission.”

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,12 @@
-export default function Progress() {
-  return null;
+export default function Progress({donations, targetAmount}) {
+  let sum = 0 
+  for(let d of donations){
+    sum+=d.amount 
+  }
+  return <section className="progress">
+  <h2>
+    Raised <span className="secondary">${sum}</span> of
+    <span className="secondary">${targetAmount}</span>
+  </h2>
+</section>
 }
