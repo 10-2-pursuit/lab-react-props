9fb208ed54453473bc345ba9d03cc209f64f3308
Addis Jackson <addisjackson@pursuit.org>
2023-07-31 15:38:03 -0400

    finishes lab

--- src/Components/DonationForm.jsx
+++ src/Components/DonationForm.jsx
@@ -1,3 +1,47 @@
-export default function DonationForm() {
-  return null;
+import React from "react";
+import donations from "../App";
+
+
+ function DonationForm({ donations }) {
+  return (
+    <section className="donation">
+      <h3>You could be donation <span className="secondary">{donation.length+1}</span></h3>  
+      {donations.map((donation) => (
+        <form key={donation.id}>
+          <label htmlFor="name">
+            Name<input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Your name..."
+            />
+            {donation.name}
+          </label>
+          <label htmlFor="caption">
+            Caption<input
+              id="caption"
+              name="caption"
+              type="text"
+              placeholder="Add a brief message..."
+            />
+            {donation.caption}
+          </label>
+          <label htmlFor="amount">
+            Amount<input
+              id="amount"
+              name="amount"
+              type="number"
+              placeholder="0"
+            />
+            ${donation.amount}
+          </label>
+          <button>Donate!</button>
+        </form>
+      ))}
+    </section>
+  );
 }
+
+export default DonationForm;
+
+
--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,17 @@
-export default function Progress() {
-  return null;
+import React from "react"
+import donations from "../App";
+
+const Progress = ({donations}) => {
+  const targetAmount = 1000;
+  const totalRaised = donations.reduce((total, donation) => total + donation.amount, 0);
+  const percentRaised = (totalRaised / targetAmount) * 100;
+  return
+  <section className="progress">
+  <h2>
+    ${totalRaised} <span className="secondary">${percentRaised.toFixed(2)}</span> of {''}
+    <span className="secondary">${targetAmount}</span>
+  </h2>
+</section>
 }
+
+export default Progress;
