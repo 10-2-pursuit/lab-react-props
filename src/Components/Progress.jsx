82ea7353dfc0e34ec8cf4c3d635df7721339aa56
Joseph-Carter <1joseph1carter@gmail.com>
2023-08-04 18:34:24 -0400

    finishe lab

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,17 @@
-export default function Progress() {
-  return null;
+import React from "react";
+
+function Progress(props) {
+  console.log(props)
+  let progressSum = 0;
+  const calculateSum = (donations) => {
+    for (let donation of donations) {
+      progressSum += donation.amount
+    }
+  }
+  const progressAmount = calculateSum(props.donations)
+
+  return (
+    <h2>Raised { progressSum } of {props.targetAmount } </h2>
+  )
 }
+export default Progress
