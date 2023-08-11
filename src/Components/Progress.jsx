e2dcde46871c182002e63c579552c81b04605fa6
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-08-11 15:55:44 -0400

    Complete Lab

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,23 @@
-export default function Progress() {
-  return null;
+import React from "react";
+
+function Progress({ targetAmount, donations }) {
+  const totalDonations = (donations) => {
+    let sum = 0;
+    for (let donation of donations) {
+      sum += parseInt(donation.amount);
+    }
+    return sum;
+  };
+  return (
+    <>
+      <section className="progress">
+        <h2>
+          Raised <span className="secondary">${totalDonations(donations)}</span> of
+          <span className="secondary">${targetAmount}</span>
+        </h2>
+      </section>
+    </>
+  );
 }
+
+export default Progress;
--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,22 @@
-export default function RecentDonations() {
-  return null;
+import React from "react";
+
+function RecentDonations({ donations }) {
+  const recentDonationsList = donations.map(donation => (
+    <li key={donation.id}>
+      <span>
+        {donation.name} donated ${donation.amount}
+      </span>
+      <span>{donation.caption}</span>
+    </li>
+  ));
+
+  return (
+    <>
+      <section>
+        <h2>Recent Donations</h2>
+        <ul>{recentDonationsList}</ul>
+      </section>
+    </>
+  );
 }
+export default RecentDonations
