0a849d8a76bf413daaac4a98f75db9864d074794
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-12-06 17:11:10 -0500

    lab...is done

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,19 @@
-export default function Progress() {
-  return null;
+
+function Progress(props) {
+  //extracting donations and targetAmount from props object
+  const {donations, targetAmount} = props
+  // calculate the total amount of money raised from the donations array 
+  const totalDonations = donations.reduce((total, donation) => total + donation.amount, 0)
+  return (
+    <div>
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${totalDonations}</span> of
+        <span className="secondary"> ${targetAmount}</span>
+      </h2>
+    </section>
+    </div>
+  )
 }
+
+export default Progress; 
--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,21 @@
-export default function RecentDonations() {
-  return null;
+import React from 'react'
+
+const RecentDonations = (props) => {
+  return (
+    <div>
+      <section>
+        <h2>Recent Donations</h2>
+        <ul>
+        {props.donations.map((donation) => (
+        <li key={donation.id}>
+        <span>${donation.name} donated ${donation.amount}
+        </span>{donation.caption}
+        </li>
+      ))}
+        </ul>
+      </section>
+    </div>
+  )
 }
+
+export default RecentDonations; 
