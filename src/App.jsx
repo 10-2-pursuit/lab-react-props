cc4968c913c041b2ddeb03877302d92aa14abb19
chris.miranda <christophermiranda@pursuit.org>
2023-12-06 17:00:26 -0500

    finished the lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
 import "./App.css";
+import RecentDonations from "./Components/RecentDonations";
+import DonationForm from "./Components/DonationForm";
+import Progress from "./Components/Progress";
 
 const targetAmount = 1000;
 const donations = [
@@ -41,10 +44,10 @@ function App() {
     <>
       <TopBar />
       <main className="container">
-        <section className="sidebar">{/* Recent Donations */}</section>
+        <section  className="sidebar"> <RecentDonations allDonations = {donations}/> </section>
         <section className="">
-          {/* Progress */}
-          {/* Donation Form */}
+          <Progress allDonations = {donations} target = {targetAmount}/>
+          <DonationForm  allDonations = {donations}/>
         </section>
       </main>
     </>
--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,10 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations(props) {
+  return (
+    <section className="sidebar">
+
+    <h2>Recent Donations</h2>
+    <ul>{props.allDonations.map((donation) => <li key= {donation.name}> <span>{donation.name} donated ${donation.amount}</span> {donation.caption} </li>)}</ul>
+
+    </section>
+  )
 }
